5507a0d3aca897f6a1bcd370d51b85907ae6313d
Asamoah Yeboah Felix <felixasamoah539@gmail.com>
2022-10-02 23:20:16 +0000

    added terms and condition

--- lib/views/signup/signup_view.dart
+++ lib/views/signup/signup_view.dart
@@ -1,3 +1,4 @@
+import 'package:doctr/theme/colors.dart';
 import 'package:doctr/views/signup/signup_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -67,6 +68,24 @@ class SignUpView extends HookWidget {
                         ),
                         const SizedBox(
                           height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(
+                                value: viewModel.agreed,
+                                onChanged: viewModel.onAgreementChanged),
+                            Expanded(
+                              child: InkWell(
+                                onTap: viewModel.onRead,
+                                child: const Text(
+                                  'I agree to the terms and conditions of the application',
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(color: primaryColor),
+                                ),
+                              ),
+                            )
+                          ],
                         ),
                         SizedBox(
                             width: double.infinity,
